f4142236e98f2903ec1e00f27b9c8c017a44091f
RajPatel7115 <rajpateliil7115@gmail.com>
2026-01-28 10:45:33 +0530

    main

---
+++ CLASSWORK/Python Language 2/Py03 - 28 - 01 - 26/Py01 - Covertion From User-input.py
@@ -0,0 +1,35 @@
+#years to days
+
+years = int(input("Enter Years : "))
+days = years * 365
+print("Total Years To Days",days)
+
+#month to years
+
+months = int(input("Enter Months : "))
+years = months / 12
+print("Total Months To Years",years)
+
+#days to weeks
+
+days = int(input("Enter Days : "))
+weeks = days / 7
+print("Total Days To Weeks",weeks)
+
+#month to days
+
+months = int(input("Enter Months : "))
+days = months * 30
+print("Total Months To Days",days)
+
+#weeks to months
+
+weeks = int(input("Enter Weeks : "))
+months = weeks * 4
+print("Total Weeks To Months",months)
+
+#weeks to year
+
+weeks = int(input("Enter Weeks : "))
+years2 = weeks / 52
+print("Total Weeks To Years",years2)
